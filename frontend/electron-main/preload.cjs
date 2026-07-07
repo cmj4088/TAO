d3e95f8a1cd0076a05482765f8cdbf82e601b3da
cmj4088 <WinansMonte282@gmail.com>
2026-07-07 10:56:06 +0800

    chore: bump version to 0.1.1 for LLM config update

--- frontend/electron-main/preload.cjs
+++ frontend/electron-main/preload.cjs
@@ -1,6 +1,14 @@
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   platform: process.platform,
-  version: "0.1.0",
+  version: "0.1.1",
+
+  onUpdateStatus: (callback) => {
+    ipcRenderer.on("update-status", (_event, data) => callback(data));
+  },
+
+  checkForUpdates: () => ipcRenderer.invoke("check-for-updates"),
+  downloadUpdate: () => ipcRenderer.invoke("download-update"),
+  quitAndInstall: () => ipcRenderer.invoke("quit-and-install"),
 });
